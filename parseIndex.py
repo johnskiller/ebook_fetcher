5fd49dfba379d339faebb35086207e1a15a2788d
John Skiller <john@skykiller.com>
2013-01-28 23:18:02 +0800

    bget.sh: add comment jiangye.sh: shell scipt for fetch 将夜 parseIndex.py: add command line params

--- parseIndex.py
+++ parseIndex.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python
 
 import re,sys
+url=''
 
 def getLastNum():
 	last = 0
+	print "sys.argv ",len(sys.argv)
 	if len(sys.argv) > 1:
 		last = int(sys.argv[1])
-
-	return last
+	if len(sys.argv) > 2:
+		url = sys.argv[2]
+		print url
+		print "last=",last,"url=",url
+	return (last,url)
 	
-def output(last):
-
+def output(last,url):
+	print "last=",last,"url=",url
+	f=open('surls.txt')
 	p=re.compile('(\d{7})\.html')
-	for line in open('allurl.txt').readlines():
+	for line in open('allurls.txt').readlines():
 		m=p.search(line)
 		if m and int(m.group(1)) > last:
-			print m.group(0), m.group(1) 
+			#print "%s/%s   %s"%(url,m.group(0), m.group(1)) 
+			f.write("%s/%s",(url,m.group(0)))
+			pass
 
+	f.close()
 
-output(getLastNum())
+last,url = getLastNum()
+output(last,url)
