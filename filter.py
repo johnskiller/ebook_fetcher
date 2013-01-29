718b124f4e88d1354da55088049567a0aedffd0d
johnskiller <johnskiller@gmail.com>
2013-01-29 17:28:39 +0800

    adj indent with tab

--- filter.py
+++ filter.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # coding=utf-8
 from bs4 import BeautifulSoup, Comment
+import re
+rot=re.compile(r'【.+】')
+
 VALID_TAGS = {'html': [],
               'head': [],
               #'p': [],
@@ -14,32 +17,38 @@ VALID_TAGS = {'html': [],
 FILE='html/4966416.html'
 
 def sanitize_html(value, valid_tags=VALID_TAGS):
-    soup = BeautifulSoup(value,'lxml')
-    comments = soup.findAll(text=lambda text:isinstance(text, Comment))
-    [comment.extract() for comment in comments]
-    # Some markup can be crafted to slip through BeautifulSoup's parser, so
-    # we run this repeatedly until it generates the same output twice.
-    newoutput = soup.renderContents()
-    while 1:
-        oldoutput = newoutput
-        soup = BeautifulSoup(newoutput,'lxml')
-        for tag in soup.findAll(True):
-            if tag.name not in valid_tags:
-                tag.hidden = True
-		tag.replaceWith('')
-            else:
-		if tag.name == 'div' and tag.attrs.get(u'id','') != u'content':
-			tag.hidden = True
-			#tag.replaceWith('')
-        newoutput = soup.renderContents()
-        if oldoutput == newoutput:
-            break
-    return newoutput
+	soup = BeautifulSoup(value,'lxml')
+	comments = soup.findAll(text=lambda text:isinstance(text, Comment))
+	[comment.extract() for comment in comments]
+	# Some markup can be crafted to slip through BeautifulSoup's parser, so
+	# we run this repeatedly until it generates the same output twice.
+	newoutput = soup.renderContents()
+	while 1:
+		oldoutput = newoutput
+		soup = BeautifulSoup(newoutput,'lxml')
+		for tag in soup.findAll(True):
+			if tag.name not in valid_tags:
+				tag.hidden = True
+				tag.replaceWith('')
+			else:
+				if tag.name == 'div' and tag.attrs.get(u'id','') != u'content':
+					tag.hidden = True
+					#tag.replaceWith('')
+					#print "--- ",tag.name, tag.attrs
+				else:
+					if tag.name == 'div' :
+						#pass
+						print "+++ ",tag.name, tag.attrs
+		newoutput = soup.renderContents()
+		if oldoutput == newoutput:
+			break
+	return newoutput
 
 def filte(file=FILE):
 	v=open('html/%s'%file).read()
 	f=open('newhtml/%s'%file,'w+')
-    	f.write(sanitize_html(v))
+	re.sub(rot,'',v)
+	f.write(sanitize_html(v))
 	f.close()
 
 def test():
@@ -49,9 +58,11 @@ def test():
 	print cont
 
 def loop_all():
-	import os
+	import os,sys
 	for f in os.listdir('html'):
 		filte(f)
+		sys.stdout.write('.')
+		sys.stdout.flush()
 
 
 loop_all()
