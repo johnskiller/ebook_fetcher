a3b751eb07945c11d067d47c61fb242f4d31d6a5
johnskiller <johnskiller@gmail.com>
2013-02-08 14:23:45 +0800

    get newest files

--- jiangye.sh
+++ jiangye.sh
@@ -2,8 +2,8 @@
 
 url='http://www.baikm.com/xiaoshuo/11/11686/'
 
-#rm -rf index.html
-#wget ${url}index.html
+rm -rf index.html
+wget ${url}index.html
 
 grep html index.html > allurls.txt
-./parseIndex.py 0 $url
+./parseIndex.py 5427700 $url
